4e5877149171727c56d95e9843befa4b79f3ea72
parisollie <pjff18@gmail.com>
2025-03-22 09:54:47 -0600

    Initial Commit

---
+++ AnimatedStateButon/AnimatedStateButonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedStateButonApp.swift
+//  AnimatedStateButon
+//
+//  Created by Paul F on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedStateButonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
